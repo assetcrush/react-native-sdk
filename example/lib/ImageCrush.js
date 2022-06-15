6ea30208bb6a4f093f574b62559d6c34ed55230e
Ali Haider <alimurtuza718@gmail.com>
2022-06-15 19:55:21 +0500

    log and key fallback added if not prod (#4)
    * log and key fallback added if not prod
    
    * updated logic
    
    * fix keys

--- example/lib/ImageCrush.js
+++ example/lib/ImageCrush.js
@@ -3,6 +3,7 @@ import { Image, ImagePropTypes } from 'react-native';
 import { getKey } from './key';
 
 const ImageCrush = (props) => {
+  const { acEnv = 'production' } = props;
   const [width, setWidth] = useState(0);
   const [height, setheight] = useState(0);
 
@@ -13,12 +14,19 @@ const ImageCrush = (props) => {
   }, [setWidth, setheight]);
 
   let newSource = undefined;
+  const key = getKey() ? getKey() : __DEV__ === "development" ? 'test-key' : '';
+
+  if (__DEV__ === "development" && key === '') {
+    console.log(' Key is not present, please go to console.assetcrush.com and sign up to get one.')
+  }
+  
   if (props.source?.uri) {
     newSource = {
       ...props.source,
       headers: {
         ...(props.source?.headers || {}),
-        ['assetcrush-key']: getKey()
+        ['assetcrush-key']: key,
+        'ac-env': acEnv
       },
       uri: width && height && props.source?.uri
         ? `https://service.assetcrush.com?width=${width}&height=${height}&original_uri=${encodeURIComponent(props.source.uri)}`
